5f347bab7f94656d757b1b333e2bc17763b74d66
Bram Christie <60450679+BramChristie@users.noreply.github.com>
2020-12-08 13:46:53 -0700

    Add files via upload

---
+++ Test (1).java
@@ -0,0 +1,14 @@
+//
+// Bram Christie
+// CSC 160 
+// Doug Lundin
+// 8/29/2020
+// 
+
+public class Test {
+  // Main method 
+  public static void main(String[] args) {
+    // Display output
+    System.out.println("Welcome to Java");
+  }
+}
